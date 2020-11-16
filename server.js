838b6ec6d3a3fdcc004b6eaeaeba915d2b2e2987
Steven <steven.gpa.git@gmail.com>
2020-11-15 18:53:59 -0600

    Adding Process Memory Details

--- server.js
+++ server.js
@@ -5,47 +5,71 @@ const numCPUs = require('os').cpus().length;
 const express = require('express');
 const cors = require('cors');
 const {
-  createLightship
+    createLightship
 } = require('lightship')
+
+const toMB = (value) => (value / 1024 / 1024).toFixed(2);
+
+function printMemory() {
+    const rss = toMB(process.memoryUsage().rss)
+    const heapTotal = toMB(process.memoryUsage().heapTotal)
+    const heapUsed = toMB(process.memoryUsage().heapUsed)
+    const external = toMB(process.memoryUsage().external)
+
+    console.log(`
+	rss: ${rss}MB\n
+	heapTotal: ${heapTotal}MB\n
+	heapUsed: ${heapUsed}MB\n
+	external: ${external}MB\n
+	`)
+}
 
 
 if (cluster.isMaster) {
-  // Fork workers.
-  for (var i = 0; i < 3; i++) {
-    cluster.fork();
-  }
+    console.log('======= Initial ==========');
+    printMemory();
+    console.log('======= End Initial ==========');
+    // Fork workers.
+    for (var i = 0; i < 3; i++) {
+        cluster.fork();
+    }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
 } else {
 
-	const lightship = createLightship()
-	const app = express();
+    const lightship = createLightship()
+    const app = express();
 
-	app.use(cors());
+    app.use(cors());
 
-	const PORT = 3001;
+    const PORT = 3001;
 
-	console.log('ENV ' + process.env.NODE_ENV);
+    console.log('ENV ' + process.env.NODE_ENV);
 
-	app.post('/hello', (req, res) => {
-		console.log('World !');
-		res.sendStatus(200).end();
-	});
+    app.post('/hello', (req, res) => {
+        console.log('World !');
+        res.sendStatus(200).end();
+    });
 
-	const httpServer = http.createServer(app);
-	const pid = process.pid;
+    app.get('/memory', (req, res) => {
+        printMemory();
+        res.sendStatus(200).end();
+    });
 
-	httpServer.listen(PORT, () => {
-		lightship.signalReady()
-		console.log(`Server running in port ${PORT} - PID ${pid}`);
-	});
+    const httpServer = http.createServer(app);
+    const pid = process.pid;
 
-	lightship.registerShutdownHandler(() => {
-	  httpServer.close();
-	});
-	 
+    httpServer.listen(PORT, () => {
+        lightship.signalReady()
+        console.log(`Server running in port ${PORT} - PID ${pid}`);
+    });
 
-	lightship.signalReady();
+    lightship.registerShutdownHandler(() => {
+        httpServer.close();
+    });
+
+
+    lightship.signalReady();
 }
